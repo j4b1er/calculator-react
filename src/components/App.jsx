8766e407b50ccd46c5c6133b07aca2d896a0f541
Javier Pardo <antonioj_2803@hotmail.com>
2023-12-07 10:43:03 -0500

    Added buttons and styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,85 @@
 import Header from "./Header";
 import Body from "./Body";
+import Button from "./Button";
+
+const buttons = [
+  {
+    name: "%",
+    action: "mod",
+  },
+  {
+    name: "÷",
+    action: "division",
+  },
+  {
+    name: "CE",
+    action: "clear",
+  },
+  {
+    name: "←",
+    action: "erase",
+  },
+  {
+    name: "7",
+    action: "num",
+  },
+  {
+    name: "8",
+    action: "num",
+  },
+  {
+    name: "9",
+    action: "num",
+  },
+  {
+    name: "X",
+    action: "multiply",
+  },
+  {
+    name: "4",
+    action: "num",
+  },
+  {
+    name: "5",
+    action: "num",
+  },
+  {
+    name: "6",
+    action: "num",
+  },
+  {
+    name: "-",
+    action: "subtract",
+  },
+  {
+    name: "1",
+    action: "num",
+  },
+  {
+    name: "2",
+    action: "num",
+  },
+  {
+    name: "3",
+    action: "num",
+  },
+  {
+    name: "+",
+    action: "addition",
+  },
+  {
+    name: "0",
+    action: "num",
+  },
+  {
+    name: ".",
+    action: "period",
+  },
+  {
+    name: "=",
+    action: "result",
+  },
+];
 
 export default function App() {
   return (
@@ -11,7 +91,11 @@ export default function App() {
           <div className="calculator__header--curr">100</div>
         </div>
       </Header>
-      <Body></Body>
+      <Body>
+        {buttons.map((btn) => (
+          <Button button={btn} key={btn.name} />
+        ))}
+      </Body>
     </div>
   );
 }
---
+++ src/components/Button.jsx
@@ -0,0 +1,10 @@
+export default function Button({ button }) {
+  return (
+    <button
+      className={`btn btn__${
+        button.name === "0" ? `${button.action}${button.name}` : button.action
+      }`}>
+      {button.name}
+    </button>
+  );
+}
