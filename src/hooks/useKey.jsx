6125e6508fed5929a22b9bdf50f4065dd44c61be
Javier Pardo <antonioj_2803@hotmail.com>
2023-12-09 08:21:46 -0500

    Improved the shift detection

--- src/hooks/useKey.jsx
+++ src/hooks/useKey.jsx
@@ -4,16 +4,12 @@ export function useKey(buttons, dispatch) {
   useEffect(() => {
     const checkKey = (e) => {
       const isShiftPressed = e.shiftKey;
-      console.log(isShiftPressed);
 
       buttons.forEach((button) => {
         button.btnkeys.forEach((key) => {
           let keyPressed = e.code.toLowerCase();
-          if (
-            isShiftPressed &&
-            (keyPressed === "digit8" || keyPressed === "equal")
-          )
-            keyPressed = `shift${keyPressed}`;
+          if (isShiftPressed) keyPressed = `shift${keyPressed}`;
+
           if (keyPressed === key.toLowerCase())
             dispatch({ type: button.action, payload: button });
         });
